6adee38207a56e894c02cd5ca90acfed4811f1dd
Rafael Alcalde Azpiazu <nekerafa@gmail.com>
2019-12-18 21:57:43 +0100

    setted initial project

---
+++ IndustryLP/IndustryLP.cs
@@ -0,0 +1,10 @@
+﻿using ICities;
+
+namespace IndustryLP
+{
+    public class IndustryLP : IUserMod
+    {
+        public string Name => "IndustryLP";
+        public string Description => "Industrial Estate Generator for Cities: Skylines using Logic Programming";
+    }
+}
