998ff4f0027eb976270719e3ee26524da8694130
Rafael Alcalde Azpiazu <nekerafa@gmail.com>
2019-12-19 18:22:12 +0100

    Added Cities Skyline assemblies for GitHub CI and added automatice mod local publish

--- IndustryLP/Properties/AssemblyInfo.cs
+++ IndustryLP/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("IndustryLP")]
 [assembly: AssemblyDescription("Industrial Estate Generator for the Videogame Cities: Skylines® using Logic Programming")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("University of A Coruña")]
 [assembly: AssemblyProduct("IndustryLP")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
 // utilizando el carácter "*", como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
